e399aef8f6897b60bb580c2dbe7003c920928099
Susanta0 <susanta721467@gmail.com>
2024-07-18 19:33:45 +0530

    changes

--- My Portfolio/index.js
+++ My Portfolio/index.js
@@ -101,3 +101,15 @@ form.addEventListener('submit', e => {
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
+
+
+
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     document.querySelector(this.getAttribute('href')).scrollIntoView({
+//       behavior: 'smooth'
+//     });
+//   });
+// });
